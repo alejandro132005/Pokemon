52f51146111a4db6b87645d9ba6be540b210fd6e
alejandro132005 <alejandrogp1395@gmail.com>
2025-03-31 17:16:07 -0500

    clase Pokemon

---
+++ src/autonoma/pokemon/models/Pokemon.java
@@ -0,0 +1,10 @@
+abstract class Pokemon {
+    int numPokdex;
+    String nombre;
+    int temporada;
+
+    // Métodos abstractos
+    abstract void atacarPlacaje();
+    abstract void atacarAriñazo();
+    abstract void atacarMordisco();
+}
